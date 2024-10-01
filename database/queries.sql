a8f57cc69440a428eb996a42064cba38831af8dc
Gavin Barro <111659306+gavin-barro@users.noreply.github.com>
2024-09-30 22:47:33 -0400

    Update queries.sql

--- database/queries.sql
+++ database/queries.sql
@@ -17,3 +17,18 @@ FROM Race_Event
 	JOIN Shipment ON Race_Event.RaceID = Shipment.RaceID
     JOIN Container ON Shipment.ShipmentID = Container.ShipmentID
 WHERE ConID = 12 AND RaceID = 57;
+
+-- Query to find all team kits that are part of a specific container
+-- in this case, container 10
+SELECT Team_Kit.*, Team_Table.TeamName
+FROM Container_contains_TeamKits
+    JOIN Team_Kit ON Container_contains_TeamKits.KitID = Team_Kit.KitID
+    JOIN Team_Table ON Team_Kit.TeamID = Team_Table.TeamID
+WHERE ConID = 10;
+
+-- Query to find the number of containers per shipment
+SELECT Shipment.ShipmentID, COUNT(Container.ConID) AS ContainerCount
+FROM Shipment
+    JOIN Container ON Shipment.ShipmentID = Container.ShipmentID
+GROUP BY Shipment.ShipmentID;
+
