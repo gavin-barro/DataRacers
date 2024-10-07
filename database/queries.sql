e9425e4d2d819a2c4068d74f62b55e80b50e7eca
Stella Lee <lee7sm@dukes.jmu.edu>
2024-10-06 20:39:09 -0400

    updated queries

--- database/queries.sql
+++ database/queries.sql
@@ -1,13 +1,14 @@
 -- query to find a team's kits (in this case the team with the ID 1234).
 SELECT *
-FROM Team_Table
-    JOIN Team_Kit ON Team_Table.TeamID = Team_Kit.TeamID
-WHERE Team_Table.TeamID = 1234;
+FROM Team
+    JOIN Kit ON Team.TeamID = Kit.TeamID
+WHERE Team.TeamID = 1234
+  AND Kit.TypeOfKit = 'Team';
 
 -- find the teams at a certain race event (race 57)
 SELECT TeamName
-FROM Team_Table
-    JOIN Race_Event ON Team_Table.RaceID = Race_Event.RaceID
+FROM Team
+    JOIN Race_Event ON Team.RaceID = Race_Event.RaceID
 WHERE RaceID = 57;
 
 
