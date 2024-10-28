2089440493444092130a3fc35da315da55bbf9ca
Austin Earl <Austin@Austins-MacBook-Pro.local>
2024-10-28 15:55:44 -0400

    Update generate.py

--- database/data/generate.py
+++ database/data/generate.py
@@ -1,9 +1,13 @@
 from faker import Faker
 
 def main():
-    fake = Faker()
-    for i in range(69):
-        print(fake.name())
+    locales = ['it_IT', 'fr_FR', 'de_DE', 'es_ES', 'nl_NL', 'pl_PL']
+    fake = Faker(locales)
+    
+    european_names = [fake.name() for _ in range(50)]
+
+    for name in european_names:
+        print(name)
 
 if __name__ == "__main__":
     main()
