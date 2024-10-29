b591300c3dc111d38814d97f82fcf65e58e800c3
daciertk <daciertk@dukes.jmu.edu>
2024-10-29 14:00:21 -0400

    Added Teams into load.sql. Updated generate to generate Team Kit Managers

--- database/pgsql/load.sql
+++ database/pgsql/load.sql
@@ -1,2 +1,3 @@
 \copy "Race_Event" FROM '../data/raceEvent.csv' CSV HEADER
 \copy "Race_Event_Organizer" FROM '../data/raceEventOrganizer.csv' CSV HEADER
+\copy "Team" FROM '../data/Team.csv' CSV HEADER
