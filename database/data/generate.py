199f64930af558e884dafce5b7275acd452c29ab
daciertk <daciertk@dukes.jmu.edu>
2024-10-29 13:27:15 -0400

    Updated generate.py to fill Person table

--- database/data/generate.py
+++ database/data/generate.py
@@ -1,13 +1,46 @@
 from faker import Faker
+import csv
+import faker
+import psycopg
+import random
+
+NUM_PEOPLE = 100 # Number of people
+STARTING_PID = 1 # Initial value of PersonID, IDs will increment incrementally from here
+STARTING_KID = 1 # Initial Value of Kit ID
+
+# Set of permissions for Persons. We can change this as needed
+permissions = ["all", "change_kits", "change_containers", "change_shipping"]
+
+
+# Connect to Database
+con = psycopg.connect(host="localhost", user="dr", dbname="dr")
+cur = con.cursor()
+
+# Initialize generators
+random.seed(0)
+fake = faker.Faker()
+
 
 def main():
     locales = ['it_IT', 'fr_FR', 'de_DE', 'es_ES', 'nl_NL', 'pl_PL']
-    fake = Faker(locales)
+    euro_fake = Faker(locales)
     
-    european_names = [fake.name() for _ in range(50)]
+    european_names = [euro_fake.name() for _ in range(50)]
 
-    for name in european_names:
-        print(name)
 
+    # Generate fake people
+    persons = []
+    j = STARTING_PID
+    for _ in range(NUM_PEOPLE):
+        person = (
+            j,
+            euro_fake.name(),
+            0, # Kit ID, Remove when we remove Kit ID from the person Table
+            euro_fake.phone_number(),
+            permissions[random.randint(0, 2)]
+        )
+        j+=1
+        cur.execute('INSERT INTO "Person" VALUES (%s, %s, %s, %s, %s)', person)
+    con.commit()
 if __name__ == "__main__":
     main()
