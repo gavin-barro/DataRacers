db5b1e9bb29a86ab69cc95fe4c09e36cd2abc778
Austin Earl <Austin@Austins-MacBook-Pro.local>
2024-10-29 15:34:10 -0400

    updated some stuff

--- database/pgsql/create_shipment.py
+++ database/pgsql/create_shipment.py
@@ -2,6 +2,9 @@ import csv
 import random
 import psycopg
 
+num_containers = 10
+num_shipments = 46
+
 def main():
 	temp_info = []
 	with open('raceEvent.csv', mode ='r')as file:
@@ -37,28 +40,45 @@ def main():
 			prev_location = line[1]
 		
 		# applies random transportation
-		temp.append(getRandomTransport())
+		temp.append(get_random_transport())
 
 		# created by
 		temp.append(int(line[3]))
 
 		# apply status
-		temp.append(getStatus())
+		temp.append(get_status())
 
 		# appends temp list to final_info list
-		print(temp)
-		cur.execute('INSERT INTO "Shipment" VALUES (%s , %s , %s, %s, %s, %s, %s)', temp)
+		#cur.execute('INSERT INTO "Shipment" VALUES (%s , %s , %s, %s, %s, %s, %s)', temp)
 	
+	container_id = 6000
+	curr_shipid = 5000
+	for i in range(num_shipments):
+		for j in range(num_containers):
+			temp = []
+			temp.append(container_id)
+			temp.append(curr_shipid)
+			temp.append(get_rand_boolean())
+			temp.append(get_random_transport())
+			temp.append(int(random.random()))
+			container_id += 1
+			cur.execute('INSERT INTO "Container" VALUES (%s, %s, %s, %s, %s)', temp)
+
+		
+		curr_shipid += 1
+
 	con.commit()
 		
+def get_rand_boolean():
+	return random.choice([True, False])
 
-def getStatus():
+def get_status():
 	return random.choice(["Arrived", "In Transit", "Not Left"])
 
-
-def getRandomTransport():
+def get_random_transport():
 	trans = ["airplane", "truck", "boat", "submarine"]
 	return random.choice(trans)
+
 
 if __name__ == "__main__":
   main()
