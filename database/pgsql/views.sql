584c126b32ce542af426efab970c5a9b069bc003
Stella Lee <lee7sm@dukes.jmu.edu>
2024-10-30 22:52:54 -0400

    new view

--- database/pgsql/views.sql
+++ database/pgsql/views.sql
@@ -15,3 +15,22 @@ WHERE
     s."Status" = 'In Transit';
 
 -- SELECT * FROM "RaceTeamShipmentView";
+
+--
+-- View UPCOMING critical shipments for a Race Event in 2024(Or specified year)
+--
+CREATE VIEW UpcomingCriticalShipments AS
+	SELECT
+	    re."RaceID",
+	    re."Location",
+	    re."Date",
+	    s."ShipmentID",
+	    s."Status" AS "Shipment Status",
+	    c."ConID" AS "Critical ContainerID"
+	FROM "Race_Event" re
+	JOIN "Shipment" s ON re."RaceID" = s."RaceID"
+	JOIN "Container" c ON s."ShipmentID" = c."ShipmentID"
+	WHERE re."Date" BETWEEN '2024-01-01' 
+		AND '2024-12-31'  
+	    AND s."Status" = 'Not Left'                      
+	    AND c."CriticalContainer" = TRUE; 
