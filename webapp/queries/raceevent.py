f7a276ad11b1a9377945824a845446c90fb53a3a
Stella Lee <lee7sm@dukes.jmu.edu>
2024-11-03 16:23:33 -0500

    queries

---
+++ webapp/queries/raceevent.py
@@ -0,0 +1,37 @@
+"""Queries for the Race Events table."""
+
+from . import db_connect
+
+
+def raceevent_all():
+    with db_connect() as conn:
+        with conn.cursor() as cur:
+            cur.execute('SELECT * FROM "Race_Event"')
+            return cur.fetchall()
+
+
+def raceevent_get(key):
+    with db_connect() as conn:
+        with conn.cursor() as cur:
+            cur.execute('SELECT * FROM "Race_Event" WHERE "RaceID" = %s', [key])
+            return cur.fetchone()
+
+
+def raceevent_ins(values):
+    with db_connect() as conn:
+        with conn.cursor() as cur:
+            cur.execute('INSERT INTO "Race_Event" VALUES '
+                        '(%s, %s, %s, %s)', values)
+            return cur.rowcount
+
+
+def raceevent_del(key):
+    with db_connect() as conn:
+        with conn.cursor() as cur:
+            cur.execute('DELETE FROM "Race_Event" WHERE "RaceID" = %s', [key])
+            return cur.rowcount
+
+
+if __name__ == "__main__":
+    print("==== First 3 Race Events ====")
+    print(raceevent_all()[:3])
