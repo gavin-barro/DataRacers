c4ac95e938ea928c00f367784f75e9268931f3b4
daciertk <daciertk@dukes.jmu.edu>
2024-11-04 15:15:00 -0500

    Commented out on-campus connection (wasnt working)

--- webapp/queries/__init__.py
+++ webapp/queries/__init__.py
@@ -2,11 +2,11 @@ import psycopg
 import socket
 
 # Determine whether connecting from on/off campus
-try:
-    socket.gethostbyname("data.cs.jmu.edu")
-    DSN = "host=data.cs.jmu.edu user=dr dbname=dr"
-except socket.gaierror:
-    DSN = "host=localhost user=dr dbname=dr"
+#try:
+#    socket.gethostbyname("data.cs.jmu.edu")
+#    DSN = "host=data.cs.jmu.edu user=dr dbname=dr"
+#except socket.gaierror:
+DSN = "host=localhost user=dr dbname=dr"
 
 
 def db_connect():
