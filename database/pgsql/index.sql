8283d3069aea539201a363575354a85f5429383e
Austin Earl <Austin@Austins-MacBook-Pro.local>
2024-11-12 14:41:43 -0500

    Update index.sql

--- database/pgsql/index.sql
+++ database/pgsql/index.sql
@@ -1,4 +1,5 @@
 CREATE INDEX race_event_raceid ON "Race_Event" ("RaceID");
 CREATE INDEX race_event_date ON "Race_Event" ("Date");
 CREATE INDEX kitweight ON "Kit" ("TotalWeight");
+CREATE INDEX kit_contains ON "Kit" ("KitID")
 
