ccb1e9b189dcb4a1fff979f123bd2a08429107ad
Gavin Barro <g.ab28320@gmail.com>
2024-11-12 14:58:52 -0500

    Update index.sql
    pushhhhh

--- database/pgsql/index.sql
+++ database/pgsql/index.sql
@@ -2,4 +2,4 @@ CREATE INDEX race_event_raceid ON "Race_Event" ("RaceID");
 CREATE INDEX race_event_date ON "Race_Event" ("Date");
 CREATE INDEX kitweight ON "Kit" ("TotalWeight");
 CREATE INDEX kit_contains ON "Kit" ("KitID")
-CREATE INDEX container_status ON "Container"("ContainerStatus")
+CREATE INDEX container_status ON "Container" ("ContainerStatus")
