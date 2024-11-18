000738fa056aab3e27bb71795215847416941091
Austin Earl <Austin@Austins-MacBook-Pro.local>
2024-11-18 17:52:48 -0500

    buttons added

--- webapp/routes/kit.py
+++ webapp/routes/kit.py
@@ -1,9 +1,7 @@
 """Routes for viewing/editing the Shipment table."""
-
 from app import app
 import queries.kit as db
 from flask import render_template, request, flash, redirect
-
 
 @app.route("/kit")
 def show_kit_all():
