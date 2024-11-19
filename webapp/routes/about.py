1fa1a4bff2e04c5aeb433f368ee0b99d30a15d8e
Austin Earl <Austin@Austins-MacBook-Pro.local>
2024-11-18 20:31:12 -0500

    pictures suck

--- webapp/routes/about.py
+++ webapp/routes/about.py
@@ -3,4 +3,5 @@ from flask import render_template, request, flash, redirect
 
 @app.route("/about")
 def about_slide():
-    return render_template("about.jinja")
+    picture_path = 'f1_logo.jpg'
+    return render_template("about.jinja", image = picture_path)
