624afed6d49a0c3d28661427fd881497c95aa990
Austin Earl <Austin@Austins-MacBook-Pro.local>
2024-12-01 19:46:08 -0500

    added kit edit

--- webapp/queries/kit.py
+++ webapp/queries/kit.py
@@ -5,3 +5,22 @@ def kit_all():
         with conn.cursor() as cur:
             cur.execute('SELECT * FROM "Kit"')
             return cur.fetchall()
+        
+def kit_get(key):
+    with db_connect() as conn:
+        with conn.cursor() as cur:
+            cur.execute('SELECT * FROM "Kit" WHERE "KitID" = %s', [key])
+            return cur.fetchone()
+        
+def kit_ins(values):
+    with db_connect() as conn:
+        with conn.cursor() as cur:
+            cur.execute('INSERT INTO "Kit" VALUES '
+                        '(%s, %s, %s, %s, %s, %s)', values)
+            return cur.rowcount
+        
+def kit_del(key):
+    with db_connect() as conn:
+        with conn.cursor() as cur:
+            cur.execute('DELETE FROM "Kit" WHERE "KitID" = %s', [key])
+            return cur.rowcount
--- webapp/routes/kit.py
+++ webapp/routes/kit.py
@@ -7,3 +7,33 @@ from flask import render_template, request, flash, redirect
 def show_kit_all():
     data = db.kit_all()
     return render_template("kit_all.jinja", data=data)
+
+
+@app.route("/kit/<key>")
+def kit_edit(key):
+    print("key", key)
+    # If the form was not submitted
+    if not request.args:
+        if key == "new":
+            values = []
+        else:
+            values = db.kit_get(key)
+        return render_template("kit_edit.jinja", key=key, values=values)
+
+    # Perform the requested action
+    values = list(request.args.values())
+    action = values.pop()
+    try:
+        if action == "Insert":
+            db.kit_ins(values)
+            flash("Kit inserted")
+        elif action == "Update":
+            db.kit_upd(key, values)
+            flash("Kit updated")
+        elif action == "Delete":
+            db.kit_del(key)
+            flash("Kit deleted")
+    except Exception as e:
+        flash(str(e))
+        return render_template("kit_edit.jinja", key=key, values=values)
+    return redirect("/kit")
